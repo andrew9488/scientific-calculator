c2b8f1a5f3ca4d18c5c85bc48d026db3ab3be049
User <pashkevichandrew@gmail.com>
2021-09-09 13:24:24 +0300

    tests

--- src/tests/tests.test.js
+++ src/tests/tests.test.js
@@ -1,6 +1,6 @@
 import AddCommand from '../Commands/AddCommand';
 
-test('sum', () => {
+test('sum should be correct', () => {
     const value = new AddCommand(3, 5).execute();
     expect(value()).toBe(5);
 });
